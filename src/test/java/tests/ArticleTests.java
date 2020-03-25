c900485ec0821e22f01396be4ad606ed9185c178
aosipets <creabin@gmail.com>
2020-03-25 03:31:56 +0300

    подготовка к дз

--- src/test/java/tests/ArticleTests.java
+++ src/test/java/tests/ArticleTests.java
@@ -1,13 +1,8 @@
 package tests;
 import lib.CoreTestCase;
-import lib.Platform;
 import lib.ui.ArticlePageObject;
-import lib.ui.MyListsPageObject;
-import lib.ui.NavigationUI;
 import lib.ui.SearchPageObject;
 import lib.ui.factories.ArticlePageObjectFactory;
-import lib.ui.factories.MyListsPageObjectFactory;
-import lib.ui.factories.NavigationUIFactory;
 import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Assert;
 import org.junit.Test;
@@ -59,57 +54,5 @@ public class ArticleTests extends CoreTestCase
 //        ArticlePageObject.presentTitleInArticle();
 //    }
     private static final String name_of_folder = "Learning programming";
-    @Test
-    public void testCheckDeletedSecondTopicAndCheckTitleFirstTopic() throws InterruptedException
-    {
-        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
-
-        SearchPageObject.initSearchInput();
-        SearchPageObject.typeSearchLine("Java");
-        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
-
-        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
-        ArticlePageObject.waitForTitleElement();
-        final String title = ArticlePageObject.getArticleTitle();
-        if (Platform.getInstance().isAndroid()){
-        ArticlePageObject.addArticleToMyList(name_of_folder);
-    }else{
-        ArticlePageObject.addArticlesToMySaved();
-    }
-        ArticlePageObject.closeArticle();
-
-        if (Platform.getInstance().isAndroid())
-        {
-            SearchPageObject.initSearchInput();
-            SearchPageObject.typeSearchLine("JavaScript");
-            SearchPageObject.clickByArticleWithSubstring("Programming language");
-            ArticlePageObject.addNewArticleToMyList(name_of_folder);
-            ArticlePageObject.closeArticle();
-        }else
-            {
-                SearchPageObject.clickByArticleWithSubstring("Programming language");
-                ArticlePageObject.addArticlesToMySaved();
-                ArticlePageObject.closeArticle();
-                SearchPageObject.clickCancelSearch();
-            }
-        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
-        NavigationUI.clickMyLists();
-
-        MyListsPageObject MyListPageObject = MyListsPageObjectFactory.get(driver);
-
-        if (Platform.getInstance().isIos())
-        {
-            MyListPageObject.clickCloseSyncPopUp();
-        }
-
-        if (Platform.getInstance().isAndroid()){
-            MyListPageObject.openFolderByName(name_of_folder);
-        }
-        String article_to_delete = "JavaScript";
-        String article_to_save = "Java";
-
-        MyListPageObject.swipeByArticleToDelete(article_to_delete);
-        MyListPageObject.waitForArticleToAppearByTitle(article_to_save);
-
-    }
+    
 }
